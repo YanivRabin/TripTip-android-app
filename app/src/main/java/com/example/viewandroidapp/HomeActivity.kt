e17cfb5b6786becf564dfa9bb96fa75241ca0d7b
YanivRabin <rabin.yaniv@gmail.com>
2024-03-01 11:53:11 +0200

    google maps implement

--- app/src/main/java/com/example/viewandroidapp/HomeActivity.kt
+++ app/src/main/java/com/example/viewandroidapp/HomeActivity.kt
@@ -2,13 +2,24 @@ package com.example.viewandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageButton
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var myMap : GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // google maps
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         // Setup navigation buttons
         val homeButton: ImageButton = findViewById(R.id.homeButton)
@@ -17,5 +28,10 @@ class HomeActivity : AppCompatActivity() {
         NavUtil.setupActivityButtons(this, homeButton, searchButton, profileButton)
     }
 
-    fun onIconCloseClick(view: View) {}
+    override fun onMapReady(googleMap: GoogleMap) {
+        myMap = googleMap
+        val sydney = LatLng(-34.0, 151.0)
+        myMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12f))
+    }
 }
