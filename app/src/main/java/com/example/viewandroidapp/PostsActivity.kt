4381e5375a37eb4fbba4eb4799a833b2cb879abd
YanivRabin <rabin.yaniv@gmail.com>
2024-03-23 17:55:33 +0200

    google places api for reviews

--- app/src/main/java/com/example/viewandroidapp/PostsActivity.kt
+++ app/src/main/java/com/example/viewandroidapp/PostsActivity.kt
@@ -1,19 +1,35 @@
 package com.example.viewandroidapp
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ImageButton
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.viewandroidapp.databinding.ActivityPostsBinding
+import com.google.android.gms.common.api.ApiException
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.FetchPlaceResponse
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
+import com.google.android.libraries.places.api.net.PlacesClient
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import com.bumptech.glide.Glide
+
 
 
 class PostsActivity : AppCompatActivity() {
 
+    private lateinit var placesClient: PlacesClient
     private lateinit var binding: ActivityPostsBinding
     private lateinit var recyclerView: RecyclerView
 
@@ -26,6 +42,95 @@ class PostsActivity : AppCompatActivity() {
         // Retrieve country name passed from HomeActivity and change the text
         val countryName = intent.getStringExtra("countryName")
         binding.countryNameTextView.text = countryName
+
+        // Show the loading indicator before starting the data loading operation
+        binding.loadingProgressBar.visibility = View.VISIBLE
+        // Hide the CardView while waiting for data
+        binding.cardView.visibility = View.GONE
+
+        // Initialize the SDK
+        Places.initializeWithNewPlacesApiEnabled(applicationContext, "AIzaSyBIKekkmwD4p8SmXo23bZtiMyn6KGtdu3o")
+        // Create a new PlacesClient instance
+        placesClient = Places.createClient(this)
+
+        // get the places from a country
+        val placesList = mutableListOf<String>()
+        val countriesAndPlaces = resources.getStringArray(R.array.places_by_country)
+        for (item in countriesAndPlaces) {
+            val countryAndPlace = item.split(",")
+            if (countryAndPlace.size == 2 && countryAndPlace[0] == countryName) {
+                placesList.add(countryAndPlace[1])
+            }
+        }
+        if (placesList.size == 2) {
+            // Generate a random index within the range of placesList size
+            val randomIndex = (0 until placesList.size).random()
+            // Get the random place using the random index
+            val randomPlace = placesList[randomIndex]
+
+            // Start the autocomplete query.
+            val autocompleteRequest = FindAutocompletePredictionsRequest.builder().setQuery(randomPlace).build()
+            // get the place ID
+            placesClient.findAutocompletePredictions(autocompleteRequest).addOnSuccessListener { response: FindAutocompletePredictionsResponse ->
+                val placeId = response.autocompletePredictions.firstOrNull()?.placeId
+                Log.d("places", placeId + " : " + response.autocompletePredictions[0].getPrimaryText(null).toString())
+
+                // Specify the fields to return.
+                val placeFields = listOf(Place.Field.REVIEWS)
+                // Construct a request object, passing the place ID and fields array.
+                val request = placeId?.let { FetchPlaceRequest.newInstance(it, placeFields) }
+                if (request != null) {
+                    placesClient.fetchPlace(request).addOnSuccessListener { response2: FetchPlaceResponse ->
+                        val reviews = response2.place.reviews
+                        if (reviews != null) {
+                            binding.loadingProgressBar.visibility = View.GONE
+                            binding.cardView.visibility = View.VISIBLE
+                            val placeReview = reviews[0]
+
+                            // Get any attribution and author attribution.
+                            val name = placeReview.authorAttribution.name + " at " + randomPlace
+                            val photo = placeReview.authorAttribution.photoUri
+                            val review = placeReview.originalText
+                            val stars = placeReview.rating.toFloat()
+                            val date = placeReview.publishTime?.subSequence(0,10)
+                            Log.d("places", "photo: $photo, name: $name, review: $review, stars: $stars, date: $date")
+
+                            if (photo != null) {
+                                val photoUri = Uri.parse(photo)
+                                Glide.with(this@PostsActivity) // Use this@PostsActivity as the context
+                                    .load(photoUri)
+                                    .placeholder(R.drawable.account_icon) // Placeholder image resource
+                                    .error(R.drawable.account_icon) // Error image resource if loading fails
+                                    .into(binding.profilePicture)
+                            }
+                            binding.name.text = name
+                            binding.ratingBar.rating = stars
+                            binding.timestamp.text = date
+                            binding.textReview.text = review
+                        }
+                        else {
+                            Log.d("places", "no reviews for that place")
+                        }
+                    }.addOnFailureListener { exception: Exception? ->
+                        binding.loadingProgressBar.visibility = View.GONE
+                        Log.d("places", "error")
+                        if (exception is ApiException) {
+                            // Handle the error.
+                            Log.d("places", "error $exception")
+                        }
+                    }
+                }
+            }.addOnFailureListener { exception: Exception? ->
+                if (exception is ApiException) {
+                    Log.e("places", "Place not found: ${exception.statusCode}")
+                }
+                displayNoReview()
+            }
+        } else {
+            Log.d("places", "this country has no reviews")
+            displayNoReview()
+        }
+
 
         // Setup RecyclerView for posts
         recyclerView = findViewById(R.id.recyclerView)
@@ -57,5 +162,14 @@ class PostsActivity : AppCompatActivity() {
             ))
         }
         return posts
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun displayNoReview() {
+        binding.loadingProgressBar.visibility = View.GONE
+        binding.cardContainer.visibility = View.GONE
+        binding.tipText2.visibility = View.VISIBLE
+        binding.tipText.text = "SORRY"
+        binding.tipText.setTextColor(ContextCompat.getColor(this, R.color.logout))
     }
 }
